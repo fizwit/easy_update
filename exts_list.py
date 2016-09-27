6c60beca877b65c0634e1727b7b1398ed99f9c67
fizwit <john@fuzzdog.com>
2016-09-27 16:16:41 -0700

    print update works

--- exts_list.py
+++ exts_list.py
@@ -6,16 +6,20 @@ import json, requests
 
 
 class exts_list(object):
-    def __init__(self, file_name, lang):
+    def __init__(self, file_name, lang, verbose=False):
         self.lang = lang
+        self.verbose = verbose
+        self.indent_n = 4
+
         self.new_exts = []
         self.exts_remove = []
         self.new_list = [] #single list of package names
-
+        self.prolog = '## remove ##\n'
+        self.indent = ' ' * self.indent_n
         eb = imp.new_module("easyconfig")
 
         self.code = 'SOURCE_TGZ  = "%(name)s-%(version)s.tgz"\n\
-SOURCE_TAR_GZ = "%(name)s-%(version)s.tar.gz"\n'
+SOURCE_TAR_GZ = "%(name)s-%(version)s.tar.gz"\n' + self.prolog
 
         with open(file_name, "r") as f:
             self.code += f.read()
@@ -23,6 +27,7 @@ SOURCE_TAR_GZ = "%(name)s-%(version)s.tar.gz"\n'
             exec (self.code, eb.__dict__)
         except  Exception, e:
             print "interperting easyconfig error: %s" % (e)
+
         self.exts_orig = eb.exts_list
         self.exts_orig2 = [item[0] for item in self.exts_orig]
         self.pkg_name = eb.name + '-' + eb.version + '-' + eb.toolchain['name'] + '-' + eb.toolchain['version']
@@ -39,28 +44,62 @@ SOURCE_TAR_GZ = "%(name)s-%(version)s.tar.gz"\n'
             else:
                 self.new_exts.append(pkg)
 
+    def pkg_match(self, indx, new_p):
+        if new_p[0] == self.exts_orig[indx][0]:
+            return True
+        else:
+            return False
+
+    def write_to_eol(self,ptr_head):
+        indx = self.code[ptr_head:].find('\n')
+        ptr_tail = ptr_head + indx + 1
+        self.out.write(self.code[ptr_head:ptr_tail])
+        return ptr_tail
+
+    def write_chunk(self,ptr_head, indx, obj_len):
+        ptr_tail = ptr_head + indx + obj_len
+        self.out.write(self.code[ptr_head:ptr_tail])
+        return ptr_tail
+
     def print_update(self):
         """ this needs to be re-written correctly
             use source text as pattern
         """
-        out = open(self.pkg_name + ".update", 'w')
-        ptr_head = 0
+        self.out = open(self.pkg_name + ".update", 'w')
+        indx = self.code.find(self.prolog)
+        ptr_head = indx + len(self.prolog)
         i = 0
         for new_p in self.new_exts:
-        indx = self.code.find("exts_list")
-        for pkg in self.new_exts:
-            if isinstance(pkg, list):
-                print "    ('%s', '%s', ext_optioins)," % (pkg[0], pkg[1])
-            else:
-                print "    '%s'," % pkg
-        print "]"
+            indx = self.code[ptr_head:].find(new_p[0])
+            if isinstance(new_p, str):  # base library with no version
+                i += 1
+                ptr_head = self.write_chunk(ptr_head,indx,len(new_p[0]))
+                continue
+            if new_p[0] == self.exts_orig[i][0]:
+                ptr_head = self.write_chunk(ptr_head, indx, len(new_p[0]))
+                indx = self.code[ptr_head:].find(self.exts_orig[i][1])
+                if new_p[1] == self.exts_orig[i][1]:
+                    ptr_head = self.write_chunk(ptr_head,indx,len(self.exts_orig[i][1]))
+                else:
+                    self.out.write(self.code[ptr_head:ptr_head + indx])
+                    self.out.write(new_p[1])
+                    ptr_head += (indx +len(new_p[1]))
+                if i < len(self.exts_orig2):
+                    i += 1
+            elif self.exts_orig[i][0] in self.new_list and (  # duplicate case
+                new_p[0] in self.exts_orig2[i:]):
+                i += 1
+                ptr_head = self.write_to_eol(ptr_head)
+            else: # new package
+                ptr_head = self.write_to_eol(ptr_head)
+                self.out.write("%s('%s', '%s', ext_options),\n" % (self.indent, new_p[0], new_p[1]))
+        self.out.write(self.code[ptr_head:])
 
     def test_pkg(self,indx, new_p):
         if new_p[0] == self.exts_orig[indx][0]:
             if new_p[1] != self.exts_orig[indx][1]:
                 print ">%20s %-15s | %20s %s" % (self.exts_orig[indx][0], self.exts_orig[indx][1],
                                                  new_p[0], new_p[1])
-                last_post = new_p[0]
             else:
                 print " %20s %-15s | %20s %s" % (self.exts_orig[indx][0], self.exts_orig[indx][1],
                                                  new_p[0], new_p[1])
@@ -68,7 +107,7 @@ SOURCE_TAR_GZ = "%(name)s-%(version)s.tar.gz"\n'
         else:
             return False
 
-    def diff_exts(self):
+    def print_diff(self):
         i = 0
         for new_p in self.new_exts:
             if isinstance(new_p, str):  #remove base libraries
@@ -99,8 +138,8 @@ class R(exts_list):
     depend_exclude = ['R', 'parallel', 'methods', 'utils', 'stats', 'stats4','graphics', 'grDevices',
                       'tools', 'tcltk']
 
-    def __init__(self, file_name):
-        exts_list.__init__(self, file_name, 'R')
+    def __init__(self, file_name, verbose=False):
+        exts_list.__init__(self, file_name, 'R', verbose)
         exts_list.url_list = R.cran_list
 
     def check_package(self, pkg_name):
@@ -122,6 +161,8 @@ class R(exts_list):
                     self.check_package(depend)
         self.new_exts.append([pkg_name, pkg_ver])
         self.new_list.append(pkg_name)
+        if self.verbose:
+            print "%20s : %s" % (pkg_name, pkg_ver)
 
 class Python_exts(exts_list):
     def __init__(self, file_name):
--- test.py
+++ test.py
@@ -2,6 +2,4 @@ from exts_list import R
 r = R('R-3.3.1-foss-2016b.eb') 
 
 r.update_exts()
-r.diff_exts()
-print "------"
-r.print_exts()
+r.print_diff()
