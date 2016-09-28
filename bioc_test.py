bd186d49b0c7b9e0c68cba4191cf75c048d0c594
fizwit <john@fuzzdog.com>
2016-09-28 16:36:42 -0700

    new files

--- bioc_test.py
+++ bioc_test.py
@@ -1,5 +1,6 @@
 from exts_list import BioC
-r = BioC('R-bundle-Bioconductor-3.3-foss-2016a-R-3.3.0.eb', verbose=True)
+#r = BioC('R-bundle-Bioconductor-3.3-foss-2016b-R-3.3.1-fh1.eb', verbose=True)
+r = BioC('R-bundle-Bioconductor-3.3-foss-2016b-R-3.3.1-fh1.eb')
 
 r.update_exts()
 r.print_update()
