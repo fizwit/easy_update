f63348d4ad9e1a3508d8f8d1f0f5e6cf5180e888
fizwit <john@fuzzdog.com>
2016-11-15 09:03:10 -0800

    clean up

--- bioc_test.py
+++
@@ -1,6 +0,0 @@
-from exts_list import R
-#r = BioC('R-bundle-Bioconductor-3.3-foss-2016b-R-3.3.1-fh1.eb', verbose=True)
-r = R('R-bundle-Bioconductor-3.3-foss-2016b-R-3.3.1-fh1.eb', verbose=True)
-
-r.update_exts()
-r.print_update()
