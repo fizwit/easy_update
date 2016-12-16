2f6969f76fdcd62c552469c4fb8cc00047212b56
fizwit <john@fuzzdog.com>
2016-12-16 09:36:59 -0800

    more tools

---
+++ GetLatestVersion.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import re
+import urllib2 
+from HTMLParser import HTMLParser
+
+
+class RParser(HTMLParser):
+    """Parse home page of Python.org and search for the latest release"""
+    def __init__(self ):
+        HTMLParser.__init__(self)
+        self.R_re = re.compile('R version (\d*).(\d*).(\d*)')
+        self.RversionSting = ''
+        self.Rversion = 0
+        self.r_data = ''
+        self.in_li  = False     # Paragraph tag
+        self.found  = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'li':
+            self.in_li = True
+
+    def handle_data(self, data):
+        if self.in_li:
+            if 'has been released' in data:
+                self.found = True 
+            self.r_data += data
+        if self.found:
+            # MAJOR.MINOR.PATCH
+            try:
+                (string,major,minor,patch) = self.R_re.match(self.r_data).group(0,1,2,3)
+                version = (int(major) * 10000) + (int(minor) * 100) + int(patch)
+                if version > self.Rversion:
+                     self.Rversion = version
+                     self.RversionSting = string
+            except AttributeError:
+                pass
+
+    def handle_endtag(self, tag):
+        if tag == 'li':
+            self.in_li = False
+            self.found = False
+            self.r_data = ''
+
+class PythonParser(HTMLParser):
+    """Parse home page of Python.org and search for the latest release"""
+    def __init__(self ):
+        HTMLParser.__init__(self)
+        self.Python2_re = re.compile('Python\s+(2).(\d*).(\d*)')
+        self.Python3_re = re.compile('Python\s+(3).(\d*).(\d*)')
+        self.Python2version = '' 
+        self.Python3version = '' 
+        self.in_p  = False     # Paragraph tag
+        self.latest = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.in_p = True
+
+    def handle_data(self, data):
+        if self.in_p:
+            if data[:7] == 'Latest:': 
+                self.latest = True 
+        if self.latest:
+            try:
+                (string,major,minor,patch) = self.Python2_re.match(data).group(0,1,2,3)
+                self.Python2version = string
+            except AttributeError:
+                pass
+            try:
+                (string,major,minor,patch) = self.Python3_re.match(data).group(0,1,2,3)
+                self.Python3version = string
+            except AttributeError:
+                pass
+
+    def handle_endtag(self, tag):
+        if tag == 'p':
+            self.in_p = False
+            self.latest = False
+
+p = PythonParser()
+f = urllib2.urlopen("https://python.org")
+html = f.read()
+p.feed(html)
+print "Latest: ", p.Python2version
+print "Latest: ", p.Python3version
+p.close()
+
+r = RParser()
+urldata = urllib2.urlopen('https://www.r-project.org/')
+html = urldata.read()
+r = RParser()
+r.feed(html)
+print "Latest: ", r.RversionSting
+r.close
+
